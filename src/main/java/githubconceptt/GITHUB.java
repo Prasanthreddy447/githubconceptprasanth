8f77db74d7d29f8d3e387eb1984daa7f118e774a
Prasanthreddy447 <prasanthreddydodda@gmail.com>
2025-06-24 19:16:57 +0530

    first commit

---
+++ src/main/java/githubconceptt/GITHUB.java
@@ -0,0 +1,38 @@
+package githubconceptt;
+
+public class GITHUB {
+	
+	/*
+	 github
+	 --------
+	                   add                   commit                          push
+	 working directory-------->staging area---------------->local repo(.git) --------->github
+	                                                                          fetch
+	                                                                          
+	  first create project in eclipse 
+	  in eclipse only we will have git in search
+	  then right click on project --> team--> share project --> create new repositary i.e local git repostiry in eclipse  
+	    select box create repo --> create repo(.git) -->finish  so now in git repo project added
+	    
+      right click on project and team--> add to index(this will go to staging)  
+      
+      now again right click on project team-->commit and add comment then it will change from staging to local git
+      
+      now after commit in local git repo under  branches master will come
+      
+      now goto git hbub and create repoosity and copy the url
+      
+      right click on project team--> remote-->push the we will get desitination repo their we will add github created url
+      
+      and give user name and password here password or github token
+      
+      we will get push specfications    then we give master  add spec  finish
+      
+      
+      
+      
+      
+      
+	 */
+
+}
